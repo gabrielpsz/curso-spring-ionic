2352c57accad4eafa321518c6da8e2575b001454
Gabriel Souza <gabrielsouza97@gmail.com>
2018-05-21 22:26:32 -0300

    Inicio domain, teste lista rest

--- src/main/java/com/cursospringionic/resources/CategoriaResource.java
+++ src/main/java/com/cursospringionic/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.cursospringionic.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursospringionic.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "teste";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Teste 01");
+		Categoria c2 = new Categoria(2, "Teste 02");
+		List<Categoria> listCategoria = new ArrayList<>();
+		listCategoria.add(c1);
+		listCategoria.add(c2);
+		return listCategoria;
 	}
 	
 }
